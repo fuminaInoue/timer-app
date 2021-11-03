66c7845512c54e0c3044c1d93ca8fc891cedea1c
fuminaInoue <fumina.mm@gmail.com>
2021-11-03 15:58:39 +0900

    material-ui導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import './App.css';
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import {index} from "components/pages"
+import { index } from 'components/pages'
 function App() {
   return (
-
     <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={index} />
-    </Switch>
+      <Switch>
+        <Route exact path="/" component={index} />
+      </Switch>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
